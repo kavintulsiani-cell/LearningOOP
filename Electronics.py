9d48303989f1e8db658f78f8ba862087eb2637ab
Kavin Tulsiani <kavintulsiani@gmail.com>
2025-11-11 16:46:44 +0530

    Order placement and receipt done

--- Electronics.py
+++ Electronics.py
@@ -9,20 +9,33 @@ class Articles :
     def get_name (self, user_id) :
         art_name = df.loc[df["id"] == self.article_id, ["name"]]
         return art_name
-class get_receipt :
-    pass
+
+    def stock_update (self, user_id):
+
+        instock = df.loc[df["id"] == user_id, ["in stock"]]
+        final_stock = instock["in stock"] - 1
+        df.loc[df["id"] == user_id, "in stock"] = final_stock
+        return (final_stock)
+
+    def get_price (self,user_id):
+        item_price = df.loc[df["id"] == user_id, ["price"]]
+        return item_price
+
 
 df = pd.read_csv("articles.csv")
 print (df)
 user_id = int(input ("Please enter the code of the article you want to purchase"))
 order_article = Articles(user_id)
 art_names = order_article.get_name (user_id)
-instock = df.loc[df["id"] == user_id, ["in stock"]]
-final_stock = instock["in stock"] - 1
-df.loc[df["id"] == user_id, "in stock"] = final_stock
+#instock = df.loc[df["id"] == user_id, ["in stock"]]
+#final_stock = instock["in stock"] - 1
+#df.loc[df["id"] == user_id, "in stock"] = final_stock#
+update_stock = order_article.stock_update (user_id)
+
 df.to_csv ("articles.csv", index=False)
-print (final_stock.squeeze())
+print (update_stock.squeeze())
 print (art_names.squeeze())
 print ("art name : ", art_names)
-price = df.loc[df["id"] == user_id, "price"].squeeze()
-create_receipt (art_names.squeeze(), price)
+#price = df.loc[df["id"] == user_id, "price"].squeeze()#
+price = order_article.get_price (user_id)
+create_receipt (art_names.squeeze(), price.squeeze())
